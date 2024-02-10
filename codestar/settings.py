4c2ff3cce923419692f075ac60489d63e5c015c0
jordana braga <danaxbraga@gmail.com>
2024-02-10 02:31:55 +0000

    Adjust file

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("9zt2m-ynuvf$k07=w^_8axt^01f(3dh53f6pk35)%%8@m!e5b4")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
